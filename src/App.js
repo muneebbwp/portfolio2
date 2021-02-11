709395f12c3914e72c98b03958939c5f012c2608
Muneeb Ali <muneebbwp@gmail.com>
2021-02-10 18:12:39 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component }  from 'react';
+import "bootstrap/dist/css/bootstrap.min.css"
+
+function App() {
+  return (
+   
+   <h2>Portfolio Project</h2>
+  );
+}
+
+export default App;
